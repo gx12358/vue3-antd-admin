4d45c2f97e78e69cf66ec557d8746a73a56d4053
gx12358 <gx12358@gmail.com>
2024-03-26 15:06:48 +0800

    core: pnpm lock 文件 npm源由腾讯切回npm

--- src/views/proComponents/layout/card/index.vue
+++ src/views/proComponents/layout/card/index.vue
@@ -1,9 +1,14 @@
+<script lang="ts" setup>
+import { GProFormSwitch } from '@gx-design-vue/pro-form'
+import { GProCard } from '@gx-design-vue/pro-card'
+</script>
+
 <template>
   <g-pro-page-container>
-    <div>111</div>
+    <GProCard title="默认尺寸" bordered style="max-width: 300px" tooltip="这是提示">
+      <template #extra>
+        <GProFormSwitch name="Enable" no-style checked-children="启用" un-checked-children="禁用" />
+      </template>
+    </GProCard>
   </g-pro-page-container>
 </template>
-
-<script lang="ts" setup>
-
-</script>
