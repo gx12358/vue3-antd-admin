7a3dc081380c97f20d52e95fbae452acf3305ce6
gx12358 <31052195+gx12358@users.noreply.github.com>
2025-01-02 14:14:21 +0800

    ✨ Update @gx-design-vue plugins and dependencies to the latest versions ♻️ Refactor build to `internal/vite-config` for `pnpm-workspace`, replace `mock` with `faker` ⚡ Adjust Pro Layout and Pro Table props, refactor mock usage and local token validation 🐛 Fix storage retrieval, token naming, and request base URL handling 🎨 Apply @antfu/eslint-config and reformat global code 📝 Update types for mock, settings, and environment variables

--- src/hooks/web/index.ts
+++ src/hooks/web/index.ts
@@ -1,7 +1,13 @@
+import usePageContent from './usePageContent'
+import useProTabel from './useProTabel'
 import useScrollPageList from './useScrollPageList'
+import useTabelState from './useTabelState'
 import useThemeStyle from './useThemeStyle'
 
 export {
+  usePageContent,
+  useProTabel,
   useScrollPageList,
+  useTabelState,
   useThemeStyle
 }
